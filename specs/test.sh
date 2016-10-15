f761c2ba4e777c36bcb8bf76be160124e67ffaaa
George Svachulay <opsforge-n7>
2016-10-15 02:06:40 +0100

    Issues in tests

--- specs/test.sh
+++ specs/test.sh
@@ -5,7 +5,11 @@ echo "Initiating tests..."
 chmod -R 0777 ./
 echo ">>> Docker Lints:"
 ./specs/opsbox.spec.sh
-echo ">>> Docker Lints concluded and none failed."
+if [ $? -eq 0 ]; then
+  echo ">>> Docker Lints concluded and none failed."
+else
+  echo ">>> Tests failed."
+fi
 echo "Initiating DockerHub builds..."
 curl --data build=true -X POST 'https://registry.hub.docker.com/u/opsforge/cloudstack/trigger/9f7ad513-f6c4-45e9-b5e6-cc977910bf9f/'
 curl --data build=true -X POST 'https://registry.hub.docker.com/u/opsforge/cloudstack-mysql/trigger/2e0c30b7-88b2-4c1a-8a55-f2cecd3fe70f/'
